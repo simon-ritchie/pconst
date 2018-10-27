0a111fca2c3d5eeb63b057dbd8b6fee181a1d1e7
simon-ritchie <antisocial.sid2@gmail.com>
2018-10-27 11:35:21 +0900

    Add PyPI setup file.

---
+++ setup.py
@@ -0,0 +1,31 @@
+# coding: UTF-8
+
+import os
+from setuptools import setup, find_package
+
+with open('./README.md', 'r') as f:
+    readme_str = f.read()
+
+setup(
+    name='pconst',
+    version='1.0.0',
+    url='https://github.com/simon-ritchie/pconst',
+    author='simon-ritchie',
+    author_email='antisocial.sid2@gmail.com',
+    maintainer='simon-ritchie',
+    maintainer_email='antisocial.sid2@gmail.com',
+    description='"pconst" library provide you const-like function on Python.',
+    long_description=readme_str,
+    packages=find_package(),
+    install_requires=[],
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+    ],
+),
