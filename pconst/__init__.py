6a27d71a95f6f17beb821a7b59c56a635cb201cb
simon-ritchie <antisocial.sid2@gmail.com>
2024-01-20 22:19:45 +0900

    #2 Add the version string to the root package and adjust build settings.

--- pconst/__init__.py
+++ pconst/__init__.py
@@ -2,3 +2,5 @@
 
 from pconst.const import Const
 const = Const()
+
+__version__: str = '1.0.1'
--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@
 
 import os
 from setuptools import setup, find_packages
+from pconst import __version__
 
 with open('./README.md', 'r') as f:
     readme_str = f.read()
 
 setup(
     name='pconst',
-    version='1.0.0',
+    version=__version__,
     url='https://github.com/simon-ritchie/pconst',
     author='simon-ritchie',
-    author_email='antisocial.sid2@gmail.com',
     maintainer='simon-ritchie',
     maintainer_email='antisocial.sid2@gmail.com',
     description='"pconst" library provide you const-like function on Python.',
@@ -20,12 +20,10 @@ setup(
     install_requires=[],
     license='MIT',
     classifiers=[
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'License :: OSI Approved :: MIT License',
     ],
 ),
